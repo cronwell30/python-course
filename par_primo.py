43e0c307e0fed8b299c5d639d20d0cd3b163164f
IgnacioMG <132957996+cronwell30@users.noreply.github.com>
2024-03-20 16:36:17 -0700

    even or odd exercise

---
+++ par_primo.py
@@ -0,0 +1,10 @@
+'''Escribir un programa que solicite al usuario un numero y mustre por pantalla
+si este numero es par o primo'''
+
+n = int(input('Digita un numero entero: '))
+
+if n % 2 == 0:
+    print('Tu numero es par')
+else:
+    print('Tu numero es primo')
+    
