90165fdcc4d6992caec642280a609b58ba63caed
IgnacioMG <132957996+cronwell30@users.noreply.github.com>
2024-03-20 16:10:53 -0700

    pyramid exercise

---
+++ piramide.py
@@ -0,0 +1,10 @@
+'''Escribir un programa que le solicite al usuario un numero entero al usuario
+y genere la siguiente figura en consola con tantos renglones cocmo el ususario
+haya seleccionado:  1
+                    1 1
+                    1 1 1
+                    1 1 1 1 '''
+
+n = int(input('Digita un numero entero: '))
+for i in range(n):
+    print('#'*(i+1))
